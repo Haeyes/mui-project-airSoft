b998c11b6b680284b3234c74740f1a4d4596fbb5
Haeyes <wrredraye@gmail.com>
2016-05-08 23:44:25 +0800

    Added files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FirstFloor.ModernUI.Windows.Controls;
+using FirstFloor.ModernUI.App.Content;
+
+namespace airSoft
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ModernWindow
+    {
+        public MainWindow()
+        {
+            SettingsAppearanceViewModel settings = new SettingsAppearanceViewModel();
+            settings.SetThemeAndColor(Properties.Settings.Default.SelectedThemeDisplayName,
+                                        Properties.Settings.Default.SelectedtThemSource,
+                                        Properties.Settings.Default.SelectedAccentColor,
+                                        Properties.Settings.Default.SelectedFontSize);
+            InitializeComponent();
+        }
+    }
+}
